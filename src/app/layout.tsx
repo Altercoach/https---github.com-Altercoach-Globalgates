a6b57b18670cda21b4f24601538eb913696f1c36
GOLDEN KEY AGENCY <goldenkeyworld@gmail.com>
2025-08-28 10:36:15 +0000

    puedes ver que es un recuadro o fondo que solo llega casi a la mitad de

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable} suppressHydrationWarning>
+    <html lang="en" className={`${inter.variable} bg-background`} suppressHydrationWarning>
       <body className="font-body antialiased">
           <SiteProvider>
             <LanguageProvider>
